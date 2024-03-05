379ff050a0ff87ef598940d750305dee73ede9be
misakuja <aniatu13@gmail.com>
2024-03-05 19:36:53 +0100

    Paranoid Commit

---
+++ src/Ex02.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Ex02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double Celsius = scan.nextDouble();
+        double Fahrenheit = ((9.0/5) * Celsius) + 32;
+        System.out.println(Fahrenheit + "F");
+    }
+}
---
+++ src/Ex03.java
@@ -0,0 +1,11 @@
+public class Ex03 {
+    public static void main(String[] args) {
+        double changingNum = 1.609;
+        int miles = 1;
+
+        for(int i = 1; i <= 10; ++i) {
+            System.out.println(miles * i + "\t" + changingNum * (double)i);
+        }
+
+    }
+}
