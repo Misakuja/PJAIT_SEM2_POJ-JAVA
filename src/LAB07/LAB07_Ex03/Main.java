15c4da31dbb5e65527086c8575f26656ac9eba8d
misakuja <aniatu13@gmail.com>
2024-04-23 13:26:53 +0200

    LAB07

--- src/LAB07/LAB07_Ex03/Main.java
+++ src/LAB07/LAB07_Ex03/Main.java
@@ -4,8 +4,8 @@ class Main {
     public static void main(String[] args) {
 
         Car1 car1 = new Car1("colour1", "model1");
-        Car1 car2 = new Car1("colour2", "model2");
-        Car1 car3 = new Car1("colour3", "model3");
+        Car2 car2 = new Car2("colour2", "model2");
+        Car3 car3 = new Car3("colour3", "model3");
 
         Car[] cars = {car1, car2, car3};
 
