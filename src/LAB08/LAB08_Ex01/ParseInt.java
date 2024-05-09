fd706af2871255747625858f08dc396d3d7d2569
misakuja <s30284@pjwstk.edu.pl>
2024-05-09 22:44:22 +0200

    LAB08 Slowly working

--- src/LAB08/LAB08_Ex01/ParseInt.java
+++ src/LAB08/LAB08_Ex01/ParseInt.java
@@ -1,4 +1,43 @@
 package LAB08.LAB08_Ex01;
 
-public class ParseInt {
+import java.util.Scanner;
+
+class ParseInt {
+    Scanner scanner = new Scanner(System.in);
+    String[] operations = {"+", "-", "/", "*"};
+
+
+    public int readNumber() {
+        System.out.println("Enter the numbers.");
+        String line = scanner.nextLine();
+
+        try {
+            return Integer.parseInt(line);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format");
+            return readNumber();
+        }
+    }
+
+    public String readOperation() {
+        System.out.println("""
+        Enter the operation.
+        + Addition
+        - Subtraction
+        / Division
+        * Multiplication""");
+        String operationInput = scanner.nextLine();
+
+        try {
+            for (String operation : operations) {
+                if (operationInput.equals(operation)) {
+                    return operationInput;
+                }
+            }
+            throw new IllegalArgumentException("Invalid operation");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid operation");
+            return readOperation();
+        }
+    }
 }
